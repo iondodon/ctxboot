1bdd01dd7a5fd86b772bd9dfeb1979e65cf8e5e8
Ion Dodon <ion.dodon@amdaris.com>
2025-04-28 11:21:15 +0300

    Support for interfaces

--- examples/di/main.go
+++ examples/di/main.go
@@ -27,24 +27,40 @@ func main() {
 		panic(err)
 	}
 
-	// Get service component
+	// Example 1: Get component by concrete type
 	service, err := cc.GetComponent(reflect.TypeOf(&UserService{}))
 	if err != nil {
 		panic(err)
 	}
-
-	// Use service
 	userService := service.(*UserService)
+	fmt.Println("Example 1 - Get by concrete type:")
 	fmt.Println(userService.GetUser("123"))
 
-	// Get database component by interface
-	db, err := cc.GetComponentByInterface(reflect.TypeOf((*database.Database)(nil)).Elem())
+	// Example 2: Get component by interface - verbose way
+	var dbInterface database.Database
+	dbType := reflect.TypeOf(&dbInterface).Elem()
+	db, err := cc.GetComponentByInterface(dbType)
 	if err != nil {
 		panic(err)
 	}
+	dbImpl := db.(database.Database)
+	dbImpl.Connect()
+	fmt.Println("\nExample 2 - Get by interface (verbose):")
+	fmt.Printf("Database connection: %s\n", dbImpl.GetConnectionString())
 
-	// Use database
-	database := db.(database.Database)
-	database.Connect()
-	fmt.Printf("Database connection: %s\n", database.GetConnectionString())
+	// Example 3: Get component by interface - concise way
+	dbImpl2 := cc.MustGetComponentByInterface(reflect.TypeOf((*database.Database)(nil)).Elem()).(database.Database)
+	dbImpl2.Connect()
+	fmt.Println("\nExample 3 - Get by interface (concise):")
+	fmt.Printf("Database connection: %s\n", dbImpl2.GetConnectionString())
+
+	// Example 4: Get component by interface - with error handling
+	dbImpl3, err := cc.GetComponentByInterface(reflect.TypeOf((*database.Database)(nil)).Elem())
+	if err != nil {
+		fmt.Printf("\nExample 4 - Error getting database: %v\n", err)
+	} else {
+		db := dbImpl3.(database.Database)
+		db.Connect()
+		fmt.Printf("\nExample 4 - Database connection: %s\n", db.GetConnectionString())
+	}
 }
